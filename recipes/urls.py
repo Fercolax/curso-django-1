572d4a1d37531d92ad63f5a943cc6fc7ea04a750
Fernando Almeida <fernando.oab81@outlook.com>
2025-04-10 10:57:44 -0300

    removido contatoe sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,6 @@ from recipes import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),  # URL for the home page
-    path('contato/', views.contato, name='contato'),  # URL for the contact page
-    path('sobre/', views.sobre, name='sobre'),  # URL for the about page
+    path('', views.home,),  # URL for the home page
+  
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/home.html')  # Render the home.html template
 
-def contato(request):
-    return HttpResponse("<h1>Contact Us</h1>")
-
-def sobre(request):
-    return HttpResponse("<h1>About Us</h1>")    
