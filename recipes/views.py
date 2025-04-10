32e7ded2897d9ada4d227a1794ff9d3fceddb439
Fernando Almeida <fernando.oab81@outlook.com>
2025-04-10 19:38:51 -0300

    criação de partials - head

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')  # Render the home.html template
+    return render(request, 'recipes/pages/home.html', context= {'name': 'Fernando Almeida'})  # Render the home.html template
 
